79615fb990595cde19aa0f1590a48bef019a0ed0
Ayushi Mathur <35095298+ayu2405@users.noreply.github.com>
2018-08-30 18:03:37 +0530

    Add files via upload

---
+++ MyList.java
@@ -0,0 +1,12 @@
+public class MyList{
+	
+	static Node createGenesisNode(){
+		Node genesisNode=new Node();
+		genesisNode.referenceNodeId=null;
+		genesisNode.timestamp=new TimeStamp();
+		
+		}
+	public static void main(){
+		
+	}
+}
---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node
+{
+	Date timestamp;
+	String data;
+	int nodeNumber;
+	String nodeId;
+	String referenceNodeId;
+	String childReferenceNodeId;
+	String genesisReferenceNodeId;
+	String hashvalue;
+}
